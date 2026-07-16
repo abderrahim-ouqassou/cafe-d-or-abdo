71c98ac5278a79faa0851faee5a9a98a72e53938
Abderrahim Ouqassou <gabrielmalma20@gmail.com>
2026-07-16 21:18:53 +0200

    Add gallery lightbox

--- script.js
+++ script.js
@@ -4,3 +4,29 @@ const navMenu = document.getElementById("nav-menu");
 menuToggle.addEventListener("click", () => {
     navMenu.classList.toggle("active");
 });
+
+
+// ================= LIGHTBOX =================
+
+const galleryImages = document.querySelectorAll(".gallery-container img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+const closeBtn = document.querySelector(".close");
+
+galleryImages.forEach(image => {
+    image.addEventListener("click", () => {
+        lightbox.style.display = "flex";
+        lightboxImg.src = image.src;
+    });
+});
+
+closeBtn.addEventListener("click", () => {
+    lightbox.style.display = "none";
+});
+
+lightbox.addEventListener("click", (e) => {
+    if (e.target === lightbox) {
+        lightbox.style.display = "none";
+    }
+});
+
